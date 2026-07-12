9a7638f2f736c881d1e77393a9319dcd27223576
Jaider J. Olivella Morales <78622710+Derjai@users.noreply.github.com>
2026-07-12 15:35:32 -0500

    feat: add setItems_visibility

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7,9 +7,7 @@ namespace enhanced_recorder
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
-        private bool _checkBoxState;
-        private Brush _color = Brushes.Blue;
+    { 
         public MainWindow()
         {
             InitializeComponent();
@@ -17,37 +15,35 @@ namespace enhanced_recorder
 
         private void OnCheckBoxState_Changed(object sender, RoutedEventArgs e) 
         {
-            switch (this.showMenu_checkBox.IsChecked)
-            {
-                case true:
-                    _checkBoxState = true;
-                    _color = Brushes.Green;
-                    break;
-                case false:
-                    _checkBoxState = false;
-                    _color = Brushes.Red;
-                    break;
-            }
-            this.summary_text.Text = $"{_checkBoxState}";
-            this.color_canva.Background = _color;
+            SetItems_Visibility((bool)this.showMenu_checkBox.IsChecked!);   
         }
+
+        private void SetItems_Visibility(bool isChecked)
+        {
+            Visibility value = isChecked ? Visibility.Hidden : Visibility.Visible;
+            this.color_canva.Visibility = value;
+            this.summary_block.Visibility = value;
+            this.summary_block.Visibility = value;
+            this.date_picker.Visibility = value;
+        }
+
 
         private void SetDate_Tomorrow() 
         {
             this.date_picker.SelectedDate = new DateTime().AddDays(1);
         }
 
-        private void start_recording_button_Click(object sender, RoutedEventArgs e)
+        private void Start_recording_button_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void stop_recording_button_Click(object sender, RoutedEventArgs e)
+        private void Stop_recording_button_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void play_recording_button_Click(object sender, RoutedEventArgs e)
+        private void Play_recording_button_Click(object sender, RoutedEventArgs e)
         {
 
         }
