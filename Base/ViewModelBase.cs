2f74fde1f6a5fcd4da1502bf88e646708b4327d6
Jaider J. Olivella Morales <78622710+Derjai@users.noreply.github.com>
2026-07-12 19:08:12 -0500

    feat: add ViewModelBase class

---
+++ Base/ViewModelBase.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace enhanced_recorder.Base;
+
+class ViewModelBase : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
