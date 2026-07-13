1c2f4cac0fc965ea064ed8e2f3e834296484330f
Jaider J. Olivella Morales <78622710+Derjai@users.noreply.github.com>
2026-07-12 19:07:59 -0500

    feat: add DelegateCommand class

---
+++ Base/DelegateCommand.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Input;
+
+namespace enhanced_recorder.Base;
+
+class DelegateCommand(Action action) : ICommand
+{
+    private readonly Action _action = action;
+    private bool _isEnabled = true;
+
+    public event EventHandler? CanExecuteChanged;
+
+    public bool CanExecute(object? parameter)
+    {
+        return _isEnabled;
+    }
+
+    public void Execute(object? parameter)
+    {
+        _action();
+    }
+
+    public bool IsEnabled
+    {
+        get { return _isEnabled; }
+        set { 
+            if (_isEnabled != value) {
+                _isEnabled = value;
+                OnCanExecuteChanged();
+            } 
+        }
+    }
+
+    private void OnCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
